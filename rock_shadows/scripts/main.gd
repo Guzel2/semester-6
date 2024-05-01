ff6152d0f2ee46bd246f2f1fdec3dd3c42b86cfc
Guzel2 <pokelau2003@gmail.com>
2024-05-01 16:26:01 +0200

    added first rock shader test

---
+++ rock_shadows/scripts/main.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+func _ready() -> void:
+	create_height_map_png()
+
+func create_height_map_png():
+	var sprite = Image.new().create(1000, 1000, false, Image.FORMAT_RGBA8)
+	
+	for x in range(200, 300):
+		for y in range(200, 300):
+			var vector = Vector2(x, y) - Vector2(250, 250)
+			var length = vector.length()
+			if length < 50:
+				var height = 1.0 - (length / 50.0)
+				
+				sprite.set_pixel(x, y, Color(height, 0, 0, 1))
+	
+	for x in range(200, 250):
+		for y in range(400, 450):
+			sprite.set_pixel(x, y, Color(.7, 0, 0, 1))
+	
+	
+	sprite.save_png("sprites/height_map.png")
+
+func create_white_square_png():
+	var sprite = Image.new().create(149, 149, false, Image.FORMAT_RGBA8)
+	
+	for x in 149:
+		for y in 149:
+			sprite.set_pixel(x, y, Color(1, 1, 1, 1))
+	
+	sprite.save_png("bg.png")
