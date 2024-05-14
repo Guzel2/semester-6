96c7acd3152d92af10512ffc43fca8dead80a226
Guzel2 <pokelau2003@gmail.com>
2024-05-14 20:22:51 +0200

    added mouse controls and updated ant

--- bachelor_project/scripts/camera.gd
+++ bachelor_project/scripts/camera.gd
@@ -10,15 +10,31 @@ var friction = .9
 
 var dir = Vector2(0, 0)
 
+var track_mouse = false
+var mouse_start_pos = Vector2(0, 0)
+var mouse_offset = Vector2(0, 0)
+
 func _input(event):
 	if event.is_action_pressed("scroll_down"):
 		change_zoom(1.0 / scroll_multiplier)
 	
 	if event.is_action_pressed("scroll_up"):
 		change_zoom(scroll_multiplier)
+	
+	if event.is_action_pressed("middle_mouse_button"):
+		track_mouse = true
+		mouse_start_pos = get_local_mouse_position()
+	if event.is_action_released("middle_mouse_button"):
+		track_mouse = false
+	
+	var mouse_event = event as InputEventMouseMotion
+	if mouse_event:
+		if track_mouse:
+			position -= mouse_event.velocity * .1
 
 func _physics_process(delta):
 	movement(delta)
+	
 
 func movement(delta):
 	var temp_dir = Vector2(0, 0)
