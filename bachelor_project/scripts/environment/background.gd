634b1ae969fd696bc95edb38857c21e8aa8f8fe5
Guzel2 <pokelau2003@gmail.com>
2024-05-18 10:18:59 +0200

    improved fake shadows

--- bachelor_project/scripts/environment/background.gd
+++ bachelor_project/scripts/environment/background.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 @export var camera : Camera
 @export var color_rect : ColorRect
 
-var sun_dir = Vector3(-1, 0, -1)
+var sun_dir = Vector3(-1, 0, -100)
 
 func _process(delta: float) -> void:
 	color_rect.material.set_shader_parameter("zoom", camera.zoom.x)
@@ -13,11 +13,11 @@ func _process(delta: float) -> void:
 	
 	#color_rect.material.set_shader_parameter("offset", (camera.position + (Vector2(500, 300) / 2) / (Vector2(500, 300) / camera.zoom)))
 	
-	var mouse_pos = get_local_mouse_position().normalized()
+	var mouse_pos = get_local_mouse_position() / 4
 	sun_dir.x = mouse_pos.x
 	sun_dir.y = mouse_pos.y
 	
-	color_rect.material.set_shader_parameter("sun_dir", sun_dir)
+	color_rect.material.set_shader_parameter("sun_dir", sun_dir.normalized())
 
 func get_local_mouse_position() -> Vector2:
 	return camera.get_local_mouse_position()
