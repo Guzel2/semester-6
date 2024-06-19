2ea0603d463aa764babebca7292b6c273671f702
Laurin Loewe <floewe@stickystonestudio.de>
2024-06-19 14:59:43 +0200

    finished basic geometrize shader

--- geometrize/scripts/basic_geometrize.gd
+++ geometrize/scripts/basic_geometrize.gd
@@ -1,20 +1,21 @@
 @tool
 extends Node2D
 
+@export var use_evolution = false
 @export var run = false:
 	set(value):
 		run = value
 		
 		if run:
-			generate_rects()
+			generate_all_rects()
 
 @export var texture : Texture2D
 var image : Image
 
 var squares
 
+var bg_rect : DrawRect
 var rects : Array = []
-var rect_count : int = 100
 
 func _process(delta):
 	if run:
@@ -24,6 +25,8 @@ func _draw():
 	if !run:
 		return
 	
+	draw_rect(bg_rect.rect, bg_rect.color)
+	
 	for drawing_rect in rects:
 		drawing_rect = drawing_rect as DrawRect
 		
@@ -32,7 +35,7 @@ func _draw():
 		
 		draw_rect(rect, color)
 
-func generate_rects():
+func generate_all_rects():
 	rects.clear()
 	
 	image = texture.get_image()
@@ -40,14 +43,167 @@ func generate_rects():
 	var width = image.get_width()
 	var height = image.get_height()
 	
-	for x in 100:
+	var total_color : Color = Color(0, 0, 0)
+	var pixel_count = width * height
+	
+	for x in width:
+		for y in height:
+			var color = image.get_pixel(x, y)
+			
+			total_color += color
+	
+	total_color /= pixel_count
+	
+	bg_rect = DrawRect.new()
+	bg_rect.position = Vector2(0, 0)
+	bg_rect.size = Vector2(width, height)
+	bg_rect.color = total_color
+	
+	var rect_count = 100
+	
+	for i in rect_count:
+		var rect = generate_rect(image)
+		rects.append(rect)
+		
+		print("Progress: ", i, "/", rect_count)
+		
+		await get_tree().create_timer(.01).timeout
+	
+	print("done")
+
+func generate_rect(image : Image) -> DrawRect:
+	var new_rects = []
+	
+	var width = image.get_width()
+	var height = image.get_height()
+	
+	for i in 15:
 		var rect = DrawRect.new()
 		
-		rect.position = Vector2(randf_range(0, width), randf_range(0, height))
-		rect.size = Vector2(randf_range(0, width - rect.position.x), randf_range(0, height - rect.position.y))
+		var found_fitting_rectangle = false
+		for test in 100:
+			
+			rect.position = Vector2(randi_range(0, width), randi_range(0, height))
+			rect.size = Vector2(randi_range(20, width - rect.position.x), randi_range(30, height - rect.position.y))
+			
+			var end = rect.position + rect.size
+			
+			if end.x < width and end.y < height:
+				found_fitting_rectangle = true
+				break
 		
-		rect.color = Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1))
+		if !found_fitting_rectangle:
+			continue
 		
-		rects.append(rect)
+		#rect.color = Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1))
+		rect.color = image.get_pixelv(rect.position + rect.size / 2)
+		
+		var total_difference = 0
+		var total_pixels = rect.size.x * rect.size.y
+		
+		for loc_x in rect.size.x - 1:
+			for loc_y in rect.size.y - 1:
+				var x = loc_x + rect.position.x
+				var y = loc_y + rect.position.y
+				
+				var color = image.get_pixel(x, y)
+				
+				var new_color = (color - rect.color)
+				
+				var difference = abs(new_color.r) + abs(new_color.g) + abs(new_color.b)
+				
+				total_difference += difference
+		
+		total_difference /= total_pixels
+		
+		rect.difference = total_difference
+		
+		for other_rect in rects:
+			rect.overlap += rect.get_overlap(other_rect)
+		
+		new_rects.append(rect)
 	
-	print(rects)
+	var min_error_size = new_rects[0].error_size
+	var return_rect = new_rects[0]
+	
+	for x in range(1, new_rects.size()):
+		var rect = new_rects[x] as DrawRect
+		
+		if rect.error_size < min_error_size:
+			return_rect = rect
+			min_error_size = rect.error_size
+	
+	if use_evolution:
+		return_rect = create_variations(image, return_rect)
+	
+	return return_rect
+
+func create_variations(image: Image, original_rect: DrawRect):
+	var new_rects = []
+	
+	var width = image.get_width()
+	var height = image.get_height()
+	
+	for i in 15:
+		var rect = DrawRect.new()
+		
+		var found_fitting_rectangle = false
+		
+		for test in 100:
+			
+			rect.position = Vector2(original_rect.position.x * randf_range(.8, 1.25), original_rect.position.y * randf_range(.8, 1.25))
+			rect.size = Vector2(original_rect.size.x * randf_range(.8, 1.25), original_rect.size.y * randf_range(.8, 1.25))
+			
+			var end = rect.position + rect.size
+			
+			if end.x < width and end.y < height:
+				found_fitting_rectangle = true
+				break
+		
+		if !found_fitting_rectangle:
+			continue
+		
+		#rect.color = Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1))
+		#rect.color = image.get_pixelv(rect.position + rect.size / 2)
+		
+		rect.color = original_rect.color# * Color(randf_range(.91, 1.1), randf_range(.91, 1.1), randf_range(.91, 1.1))
+		
+		var total_difference = 0
+		var total_pixels = rect.size.x * rect.size.y
+		
+		for loc_x in rect.size.x - 1:
+			for loc_y in rect.size.y - 1:
+				var x = loc_x + rect.position.x
+				var y = loc_y + rect.position.y
+				
+				var color = image.get_pixel(x, y)
+				
+				var new_color = (color - rect.color)
+				
+				var difference = abs(new_color.r) + abs(new_color.g) + abs(new_color.b)
+				
+				total_difference += difference
+		
+		total_difference /= total_pixels
+		
+		rect.difference = total_difference
+		
+		for other_rect in rects:
+			rect.overlap += rect.get_overlap(other_rect)
+		
+		new_rects.append(rect)
+	
+	var min_error_size = original_rect.error_size
+	var return_rect = original_rect
+	
+	for x in range(1, new_rects.size()):
+		var rect = new_rects[x] as DrawRect
+		
+		if rect.error_size < min_error_size:
+			return_rect = rect
+			min_error_size = rect.error_size
+	
+	if return_rect == original_rect:
+		print("chose original")
+	
+	return return_rect
--- geometrize/scripts/draw_rect.gd
+++ geometrize/scripts/draw_rect.gd
@@ -5,12 +5,41 @@ var position : Vector2:
 	set(value):
 		position = value
 		rect.position = value
+		
+		set_end()
 
 var size : Vector2:
 	set(value):
 		size = value
 		rect.size = value
+		
+		set_end()
+
+var end : Vector2
 
 var rect : Rect2
 
 var color : Color
+
+var difference : float = 0
+var overlap : int = 0
+var error_size : float:
+	get:
+		return difference + float(overlap) / 1500 + (6000 / (size.y * size.y))
+
+func set_end():
+	end = position + size
+
+func get_overlap(other_rect : DrawRect) -> int:
+	var overlap_left = max(position.x, other_rect.position.x)
+	var overlap_right = min(end.x, other_rect.end.x)
+	var overlap_top = max(position.y, other_rect.position.y)
+	var overlap_bottom = min(end.y, other_rect.end.y)
+	
+	var overlap_width = overlap_right - overlap_left
+	var overlap_height = overlap_bottom - overlap_top
+	
+	if overlap_width > 0 and overlap_height > 0:
+		return overlap_width * overlap_height
+	else:
+		return 0
